9dd6fad576547940cdcc5ae268752964ed5d17ea
Markus Lykonhold <lykonhold@gmail.com>
2022-01-29 21:05:25 +0100

    why me

---
+++ src/extensions/markusStyle/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IMarkusStyleApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'MarkusStyleApplicationCustomizerStrings' {
+  const strings: IMarkusStyleApplicationCustomizerStrings;
+  export = strings;
+}
